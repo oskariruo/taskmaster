9a2f6f9f4ee06b3da5076ab2f306c28fcc4f8604
Oskari Ruotsalainen <78789083+oskariruo@users.noreply.github.com>
2024-01-30 10:22:52 +0200

    Nav.js formatting and comments

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { AppBar, Button, Toolbar } from '@mui/material';
-import useAuth from '../auth/auth';
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AppBar, Button, Toolbar } from "@mui/material";
+import useAuth from "../auth/auth";
 export default function Nav({ toggleTheme }) {
-  
+  //authentication and loggin out from useauth
   const { authenticated, handleLogout } = useAuth();
   const navigate = useNavigate();
- 
-  console.log('Authenticated:', authenticated);
-  
+  // Logging the current authentication status.
+  console.log("Authenticated:", authenticated);
+
+  //handling logout attempt
   const handleLogoutAttempt = async () => {
     try {
       await handleLogout();
 
-      navigate('/login');
+      navigate("/login");
     } catch (error) {
-      console.error('Error during logout:', error);
+      console.error("Error during logout:", error);
     }
   };
   return (
-    <AppBar position='static'>
-      <Toolbar sx={{ justifyContent: 'center' }}>
+    <AppBar position="static">
+      <Toolbar sx={{ justifyContent: "center" }}>
         <Button color="inherit" component={Link} to="/yesterday">
           Yesterday
         </Button>
@@ -33,7 +34,9 @@ export default function Nav({ toggleTheme }) {
         <Button onClick={toggleTheme} color="inherit">
           Theme
         </Button>
-        <Button color="inherit" onClick={handleLogoutAttempt}>Log out!</Button>
+        <Button color="inherit" onClick={handleLogoutAttempt}>
+          Log out!
+        </Button>
       </Toolbar>
     </AppBar>
   );
